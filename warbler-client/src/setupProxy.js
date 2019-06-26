233dfd3299b468fbb347f13346223bde6ccdbb16
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-06-26 16:48:07 -0400

    removed uncessary dependency

--- warbler-client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require('http-proxy-middleware');
-const proxyUrl =
-	process.env.NODE_ENV === 'production'
-		? `http://localhost:${process.env.PORT}`
-		: 'http://localhost:8081';
-
-module.exports = function(app) {
-	app.use(proxy('/api/', { target: proxyUrl }));
-};
